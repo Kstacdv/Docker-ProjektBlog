44c56360ba64ded396cdd66dcb25acf2aa520e3f
Kstacdv <kstachowski2@edu.cdv.pl>
2025-12-14 12:16:18 +0100

    migration and update to CreateUser

--- src/Command/CreateUserCommand.php
+++ src/Command/CreateUserCommand.php
@@ -16,8 +16,11 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 )]
 class CreateUserCommand extends Command
 {
-    public function __construct()
-    {
+
+    private bool $requirePassword;
+
+    public function __construct(bool $requirePassword = false) {
+        $this->requirePassword = $requirePassword;
         parent::__construct();
     }
 
@@ -26,6 +29,7 @@ class CreateUserCommand extends Command
         $this
             ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
             ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+            ->setHelp("This command allows you to create a user");
         ;
     }
 
@@ -34,15 +38,27 @@ class CreateUserCommand extends Command
         $io = new SymfonyStyle($input, $output);
         $arg1 = $input->getArgument('arg1');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
-        }
+        $output->writeln([
+           'User Creator',
+           '============='
+        ]);
 
-        if ($input->getOption('option1')) {
-            // ...
-        }
+//        $output->writeln($this->someMethod());
 
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        $output->writeln('Whoa!');
+
+        $output->write('You are about to ');
+        $output->write('create user.');
+
+//        if ($arg1) {
+//            $io->note(sprintf('You passed an argument: %s', $arg1));
+//        }
+//
+//        if ($input->getOption('option1')) {
+//            // ...
+//        }
+//
+//        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
 
         return Command::SUCCESS;
     }
