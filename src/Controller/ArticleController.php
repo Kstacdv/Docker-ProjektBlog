f7d44130c851c9fd24174a7c157a3b348e69f17b
Kapcer17 <95164009+Kapcer17@users.noreply.github.com>
2026-01-21 23:43:41 +0100

    Wyświetlanie artykułów + menu nawigacyjne dla base.html ++mała poprawka z searchbarem +++ przygotowanie pod kreator artykułów dla użytkowników

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    #[Route('/article/{id}', name: 'app_article_show', methods: ['GET'])]
+    public function show(Article $article): Response
+    {
+        return $this->render('article/show.html.twig', [
+            'article' => $article,
+        ]);
+    }
+
+    #[Route('/article/new', name: 'app_article_new')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article->setDateAdded(new \DateTime());
+            $article->setAuthor($this->getUser());
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('main_page');
+        }
+
+        return $this->render('article/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+}
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Formatter\ApiResponseFormatter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
 
-class UserController
+class UserController extends AbstractController
 {
     #[Route('api/users/show', name: 'app_user', methods: ['GET'])]
     public function showUser(ApiResponseFormatter $formatter): JsonResponse {
